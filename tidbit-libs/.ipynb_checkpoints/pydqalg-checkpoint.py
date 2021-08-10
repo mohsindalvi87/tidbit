b3e6697eca7352cf1ff423aa54822ddf1989f08a
mohsindalvi87 <mohsindalvi87@gmail.com>
2021-08-10 23:17:28 +0530

    revamped pydqalg

--- tidbit-libs/.ipynb_checkpoints/pydqalg-checkpoint.py
+++ tidbit-libs/.ipynb_checkpoints/pydqalg-checkpoint.py
@@ -29,9 +29,9 @@ def v2mskew(v):
 
 
 class Quat:
-    def __init__( self, in_s=1, in_v=[0,0,0] ):
-        self.s = in_s
-        self.v = np.copy(in_v).T
+    def __init__( self, in_s=1.0, in_v=[0.0,0.0,0.0] ):
+        self.s = float(in_s)
+        self.v = np.copy(in_v).astype(float)
 
     def __repr__(self):
         return 'Quat(%f, [%f, %f, %f])' % \
@@ -57,6 +57,14 @@ class Quat:
     
     def __rmul__(self, scalar):
         return Quat( scalar*self.s, scalar * self.v )
+    
+    def mulML(self):
+        return np.vstack(( np.hstack(( self.s, -self.v.T )), np.hstack(( \
+               self.v.reshape(3,1), self.s*np.eye(3) + v2mskew(self.v) )) ))
+    
+    def mulMR(self):
+        return np.vstack(( np.hstack(( self.s, -self.v.T )), np.hstack(( \
+               self.v.reshape(3,1), self.s*np.eye(3) - v2mskew(self.v) )) ))
 
     def conj(self):
         return Quat( self.s, -self.v )
@@ -66,21 +74,21 @@ class Quat:
     
     def unit(self):
         qnorm = self.norm()
-        if qnorm > 1e-5:
-#             return Quat( self.s/qnorm, self.v/qnorm )
-            return (1/qnorm) * self
-        else:
-            return Quat( 0, [0,0,0] )
+        is_eps = qnorm < 1e-5
+        return Quat(0, [0,0,0]) if is_eps==True else (1/qnorm)*self
     
     def inv(self):
         sumsquare = self.s*self.s + np.sum(self.v*self.v)
-        if sumsquare > 1e-9:
-            return (1/sumsquare) * self.conj()
-        else:
-            return Quat( 0, [0,0,0] )
+        is_eps = sumsquare < 1e-9
+        return Quat(0, [0,0,0]) if is_eps==True else (1/sumsquare)*self.conj()
 
     def dot(p):
-        return 0.5 * ( p.conj()*q + q.conj()*p )
+#         return 0.5 * ( p.conj()*q + q.conj()*p )
+        return (self.p*p.s + np.dot(self.v,p.v), np.zeros(3) )
+        
+    def cross(p):
+#         return 0.5 * ( p*q - q.conj()*p.conj() )
+        return (0, self.s*p.v + p.s*self.v + np.cross(self.v,p.v) )
         
     def iden():
         return Quat([1, 0, 0, 0])
@@ -93,13 +101,15 @@ class Quat:
         return axis, angle
     
     def q2rotmat(self):
-        u = self.unit()
-        return 2 * np.dot(u.v.T, u.v) + (2*u.s*u.s - 1) * np.eye(3) \
+        u = self.unit();    uvT, uv = np.meshgrid(u.v, u.v)
+#         ra=2*(uvT*uv);  rb=(2*u.s*u.s-1)*np.eye(3);  rc=2*u.s*v2mskew(u.v)
+#         print(ra, rb, rc)
+        return 2 * np.multiply(uvT, uv) + (2*u.s*u.s - 1) * np.eye(3) \
                 + (2 * u.s * v2mskew(u.v))
     
-    def q2rollpitchyaw(self):
+    def q2YPR(self):
         R = self.q2rotmat()
-        return rotmat2rollpitchyaw(R)
+        return rotmat2YPR(R)
 
 
 ##########################################
@@ -107,94 +117,91 @@ class Quat:
 
 def rotmat2q(R):
     s = np.sqrt( R[0,0] + R[1,1] + R[2,2] + 1 ) / 2
-    v0 = (R[2,1] - R[1,2] ) / 2
-    v1 = (R[0,2] - R[2,0] ) / 2
-    v2 = (R[1,0] - R[0,1] ) / 2
-    return Quat(s, [v0, v1, v2])
-
-def rotmat2rollpitchyaw(R):
-    r = np.arctan2( R[1,0], R[0,0] )
-    y = np.arctan2( R[2,1], R[2,2] )
-    p = np.arcsin( -R[2,0])
-    return r, p, y
+    print(s)
+    v0 = (R[2,1]-R[1,2]) / (2*s)
+    v1 = (R[0,2]-R[2,0]) / (2*s)
+    v2 = (R[1,0]-R[0,1]) / (2*s)
+    return Quat(s, [v0,v1,v2])
     
-def rollpitchyaw2q(r, p, y):
+def YPR2q(y, p, r):
     Cr = np.cos(r) ;  Cp = np.cos(p) ;  Cy = np.cos(y)
     Sr = np.sin(r) ;  Sp = np.sin(p) ;  Sy = np.sin(y)    
     return Quat( Cy*Cp*Cr-Sy*Sp*Sr, [ Sy*Cp*Cr-Cy*Sp*Sr, \
-                  Cy*Sp*Cr-Sy*Cp*Sr,  Cy*Cp*Sr-Sy*Sp*Cr ] ) 
+                  Cy*Sp*Cr-Sy*Cp*Sr, Cy*Cp*Sr-Sy*Sp*Cr ] ) 
 
-def rollpitchyaw2rotmat(r, p, y):
+def YPR2rotmat(y, p, r):
     Cr = np.cos(r) ; Cp = np.cos(p) ; Cy = np.cos(y)
     Sr = np.sin(r) ; Sp = np.sin(p) ; Sy = np.sin(y)
     return np.array([ [Cr*Cp, Cr*Sp*Sy-Sr*Cy, Cr*Sp*Cy+Sr*Sy],
                       [Sr*Cp, Sr*Sp*Sy+Cr*Cy, Sr*Sp*Cy-Cr*Sy],
                       [  -Sp,          Cp*Sy,          Cp*Cy] ]) - np.eye(3)
 
-# def scal2quat(scalar):
-#     return Quat(scalar*np.eye(4))
+def rotmat2YPR(R):
+    y = np.arctan2( R[2,1], R[2,2] )
+    p = np.arctan2( -R[2,0], np.sin(y)*R[2,1] + np.cos(y)*R[2,2] )
+    r = np.arctan2( R[1,0], R[0,0] )
+#     p = np.arcsin( -R[2,0])
+    return y, p, r
 
-# def quat2htm(q):
-#     q = np.array(q).T
-#     return  0 - np.eye(3) + 2 * np.array( [
-#         [ q[1]*q[1]+q[0]*q[0], q[1]*q[2]-q[0]*q[3], q[1]*q[3]+q[0]*q[2] ],
-#         [ q[2]*q[1]+q[0]*q[3], q[2]*q[2]+q[0]*q[0], q[2]*q[3]-q[0]*q[1] ],
-#         [ q[3]*q[1]-q[0]*q[2], q[3]*q[2]+q[0]*q[1], q[3]*q[3]+q[0]*q[0] ] ] )
 
 
 ##########################################
 
 
-class DualQuat:
-    def __init__( self, p = [1, 0, 0, 0, 0, 0, 0, 0] ):
-        self.q = np.array(p, dtype=float)
-        self.R = Quat(p[0:4])
-        self.D = Quat(p[4:8])
+class DualQuat(Quat):
+    def __init__( self, in_Rl = Quat(1, [0,0,0]), in_Dl = Quat(0, [0,0,0]) ):
+        self.Rl = in_Rl  # super().__init__(in_Rl)
+        self.Dl = in_Dl  # super().__init__(in_Dl)
     
     def __repr__(self):
-        return 'DualQuat[%f, %f, %f, %f; %f, %f, %f, %f]' % \
-            (self.q[0], self.q[1], self.q[2], self.q[3], \
-             self.q[4], self.q[5], self.q[6], self.q[7],)
+        return 'DualQuat(Quat(%f, [%f, %f, %f])\n \
+        + \u03B5 Quat(%f, [%f, %f, %f]))' % \
+            (self.Rl.s, self.Rl.v[0], self.Rl.v[1], self.Rl.v[2], \
+             self.Dl.s, self.Dl.v[0], self.Dl.v[1], self.Dl.v[2],)
         
     def __str__(self):
-        return 'DualQuat[%f, %f, %f, %f; %f, %f, %f, %f]' % \
-            (self.q[0], self.q[1], self.q[2], self.q[3], \
-             self.q[4], self.q[5], self.q[6], self.q[7],)
+        return 'DualQuat(Quat(%f, [%f, %f, %f])\n \
+        + \u03B5 Quat(%f, [%f, %f, %f]))' % \
+            (self.Rl.s, self.Rl.v[0], self.Rl.v[1], self.Rl.v[2], \
+             self.Dl.s, self.Dl.v[0], self.Dl.v[1], self.Dl.v[2],)
         
     def __add__(self, p):
-        return DualQuat(self.q + p.q)
+        return DualQuat(self.Rl+p.Rl, self.Dl+p.Dl)
         
     def __sub__(self, p):
-        return DualQuat(self.q - p.q)
+        return DualQuat(self.Rl-p.Rl, self.Dl-p.Dl)
         
     def __mul__(self, p):
-        real = self.R * p.R
-        dual = self.R * p.D + self.D * p.R
-        return DualQuat(np.hstack(( real.q, dual.q )))
+        real = self.Rl*p.Rl
+        dual = self.Rl*p.Dl + self.Dl*p.Rl
+        return DualQuat( real, dual )
     
     def __rmul__(self, scalar):
-        return DualQuat( scalar * self.q )
+        return DualQuat( scalar*self.Dl, scalar*self.Rl )
         
     def dconj(self):
-        return DualQuat( np.hstack(( self.R.q, -self.D.q )) )
+        return DualQuat( self.Rl, -1*self.Dl )
     
     def qconj(self):
-        return DualQuat( np.hstack(( self.R.q, self.D.conj().q )) )
+        return DualQuat( self.Rl.conj(), self.Dl.conj() )
     
     def dqconj(self):
-        return DualQuat( np.hstack(( self.R.q, -self.D.conj().q )) )
+        return DualQuat( self.Rl.conj(), -1*self.Dl.conj() )
     
     def inv(self):
-        Rpart = self.R.inv()
-        Dpart = Rpart.smult(-1) * self.D * Rpart
-        return DualQuat( np.hstack(( Rpart.q, Dpart.q )) )
+        Rpart = self.Rl.inv()
+        Dpart = -1 * Rpart * self.Dl * Rpart
+        return DualQuat( Rpart, Dpart )
     
+    def dqiden():
+        return DualQuat( Quat(1, [0,0,0]), Quat(0, [0,0,0]) )
+
     def swap(self):
-        return DualQuat( np.hstack(( self.D.q, self.R.q )) )
+        return DualQuat( self.Dl, self.Rl )
     
-    def angleaxistran(self):
-        angle, axis = self.R.angleaxis()
-        tran = Quat( 2 * self.D.q * self.R.conj() ).q[1:4]
+    def dq2angleaxisdist(self):
+        angle, axis = self.Rl.angleaxis()
+        tran = Quat( 2 * self.Dl * self.Rl.conj() ).v
         return angle, axis, tran
     
 #     def norm(self):
@@ -204,28 +211,38 @@ class DualQuat:
 #         return DualQuat(self.q / self.norm())
     
     def dq2htm(self):
-        tv = Quat( 2 * self.D.q * self.R.conj() ).q[1:4]
-        row_123 = np.hstack(( np.self.R.q2rotmat(), tv ))
+        tv = Quat( 2 * self.Dl * self.Rl.conj() ).v
+        row_123 = np.hstack(( self.Rl.q2rotmat(), tv ))
         return np.vstack(( row_123, np.array([0, 0, 0, 1]) ))
-        
-    def dqiden():
-        return DualQuat([1, 0, 0, 0, 0, 0, 0, 0])
 
     
 ##########################################
 
-    
-def join(Rpart, Dpart):
-    return DualQuat( np.hstack(( Rpart, Dpart )) )
-    
 def rqtq2dq(rotq, transq):
     dualpart = (0.5 * transq) * rotq.conj()
-    return join( Rotq.q, dualpart.q )
+    return DualQuat( Rotq, dualpart )
     
 def htm2dq(T):
     tq = Quat(np.hstack(( 0, T[0:4,3] )))
-    R = Quat.rot2q( T[0:4,0:4] )
-    return DualQuat.rottrans2dq( R, tq )
+    rq = Quat.rot2q( T[0:4,0:4] )
+    return DualQuat.rqtq2dq( rq, tq )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ##########################################
